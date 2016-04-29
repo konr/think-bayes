b3d2ad30d854b4c128cdd10832e78eb10d10e9ae
Konrad Scorciapino <konr@konr.mobi>
2016-04-28 23:19:16 -0300

    Adding deps

--- project.clj
+++ project.clj
@@ -1,6 +1,20 @@
 (defproject think-bayes "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :description "Think Bayes book code implemented in Clojure"
+  :license {:name "GPL 3"}
+
+  :plugins [[lein-midje "3.1.3"]
+            [lein-cloverage "1.0.6"]
+            [lein-vanity "0.2.0"]
+            [s3-wagon-private "1.2.0"]
+            [lein-ancient "0.6.7"]]
+
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [prismatic/schema "1.0.4"]
+                 [prismatic/plumbing "0.5.0"]]
+
+  :profiles {:dev {:source-paths ["config"]
+                   :dependencies [[midje "1.7.0" :exclusions [org.clojure/clojure]]
+                                  [org.clojure/tools.namespace "0.2.11"]
+                                  [ns-tracker "0.3.0"]]}}
+
+  )
